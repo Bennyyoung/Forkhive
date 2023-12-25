025e3abaf1fb0eeac2495d88df53d9b36ebc342d
Bennyyoung <benjamineffiong124@yahoo.com>
2023-12-25 20:20:14 +0100

    fix: Worked on the Tooltip in PriceComparisonTable.tsx

--- src/components/PriceComparisonTable/PriceComparisonTable.tsx
+++ src/components/PriceComparisonTable/PriceComparisonTable.tsx
@@ -19,137 +19,192 @@ function PriceComparisonTable() {
 
                     <tbody>
                         <tr>
-                            <td><a data-tip data-for="socialMedia">Integration of Social Media (<i className="fa fa-facebook-f"></i> <i className="fa fa-twitter"></i> <i className="fa fa-instagram"></i> <i className="fa fa-whatsapp"></i>)
+                            <td>
+                                <a className='socialMedia'>
+                                    Integration of Social Media (<i className="fa fa-facebook-f"></i> <i className="fa fa-twitter"></i> <i className="fa fa-instagram"></i> <i className="fa fa-whatsapp"></i>)
 
-                                <i className="fa fa-question-circle"></i>
-                            </a>
-                                <Tooltip style={{ maxWidth: '95%' }} id="socialMedia">
-                                    <p>Get found on social media</p>
-                                </Tooltip>
+                                    {' '}<i className="fa fa-question-circle"></i>
+                                </a>
+                                <Tooltip
+                                    anchorSelect='.socialMedia'
+                                    content='Get found on social media'
+                                    style={{ width: '15rem' }}
+                                />
                             </td>
                             <td><i className="fa fa-check"></i></td>
                             <td><i className="fa fa-check"></i></td>
                         </tr>
 
                         <tr>
-                            <td><a data-tip data-for="ssl_tsl">SSL/TLS Certificate
-                                <i className="fa fa-question-circle"> </i>
-                            </a>
-                                <Tooltip style={{ maxWidth: '95%' }} id="ssl_tsl">
-                                    <p style={{ maxWidth: '95%' }}>If your site has a login or accepts<br /> form submissions, HTTPS is essential for <br /> your users’ security and privacy. Google search results<br /> prioritize sites with HTTPS enabled</p>
-                                </Tooltip>
+                            <td>
+                                <a className='ssl_tsl'>SSL/TLS Certificate
+                                    {' '}<i className="fa fa-question-circle"></i>
+                                </a>
+                                <Tooltip
+                                    anchorSelect='.ssl_tsl'
+                                    content='If your site has a login or accepts form submissions, HTTPS is essential for your users’ security and privacy. Google search results prioritize sites with HTTPS enabled'
+                                    style={{ width: '15rem' }}
+                                />
                             </td>
                             <td><i className="fa fa-check"></i></td>
                             <td><i className="fa fa-check"></i></td>
                         </tr>
 
                         <tr>
-                            <td><a data-tip data-for="fee">Hosting Fee <i className="fa fa-question-circle"></i>
-                            </a>
-                                <Tooltip id="fee">
-                                    <p>Hosting fee is free</p>
-                                </Tooltip>
+                            <td>
+                                <a className="fee">Hosting Fee
+                                    {' '}<i className="fa fa-question-circle"></i>
+                                </a>
+                                <Tooltip
+                                    anchorSelect='.fee'
+                                    content="Hosting fee is free"
+                                    style={{ width: '15rem' }}
+
+                                />
                             </td>
                             <td><i className="fa fa-check"></i></td>
                             <td><i className="fa fa-check"></i></td>
                         </tr>
 
                         <tr>
-                            <td><a data-tip data-for="cms">CMS (Content Management System) <i className="fa fa-question-circle"></i>
-                            </a>
-                                <Tooltip id="cms">
-                                    <p>Would you love to create, manage, and modify <br />content on your website without the need for<br /> our specialized technical knowledge</p>
-                                </Tooltip>
+                            <td>
+                                <a className="cms">CMS (Content Management System)
+                                    {' '}<i className="fa fa-question-circle"></i>
+                                </a>
+                                <Tooltip
+                                    anchorSelect='.cms'
+                                    content="Would you love to create, manage, and modify content on your website without the need for our specialized technical knowledge"
+                                    style={{ width: '15rem' }}
+
+                                />
                             </td>
                             <td><i className="fa fa-check"></i></td>
                             <td><i className="fa fa-check"></i></td>
                         </tr>
 
                         <tr>
-                            <td><a data-tip data-for="cdr">Custom Domain Registration
-                                <i className="fa fa-question-circle"></i>
-                            </a>
-                                <Tooltip id="cdr">
-                                    <p>Register a custom address like www.YourBusinessOrName.com <br />to your website or online store.</p>
-                                </Tooltip>
+                            <td>
+                                <a className="cdr">Custom Domain Registration
+                                    {' '}<i className="fa fa-question-circle"></i>
+                                </a>
+                                <Tooltip
+                                    anchorSelect='.cdr'
+                                    content='Register a custom address like www.YourBusinessOrName.com to your website or online store.'
+                                    style={{ width: '15rem' }}
+
+                                />
                             </td>
                             <td><i className="fa fa-check"></i></td>
                             <td><i className="fa fa-check"></i></td>
                         </tr>
 
                         <tr>
-                            <td><a data-tip data-for="form">Form Submission <i className="fa fa-question-circle"></i>
-                            </a>
-                                <Tooltip id="form">
-                                    <p>Netlify build bots do it by parsing your HTML files directly at deploy time, so there’s no need for us to make an API call or include extra JavaScript on your site</p>
-                                </Tooltip>
+                            <td>
+                                <a className="form_">Form Submission
+                                    {' '}<i className="fa fa-question-circle"></i>
+                                </a>
+                                <Tooltip
+                                    anchorSelect='.form_'
+                                    content='Netlify build bots do it by parsing your HTML files directly at deploy time, so there’s no need for us to make an API call or include extra JavaScript on your site'
+                                    style={{ width: '15rem' }}
+
+                                />
                             </td>
                             <td><i className="fa fa-check"></i></td>
                             <td><i className="fa fa-check"></i></td>
                         </tr>
 
                         <tr>
-                            <td><a data-tip data-for="support">
-                                24/7 Support <i className="fa fa-question-circle"></i>
-                            </a>
-                                <Tooltip id="support">
-                                    <p>Friendly Forkhive Guides are standing by to answer your questions. Call +234 703 164 2200.</p>
-                                </Tooltip>
+                            <td>
+                                <a className="support">
+                                    24/7 Support {' '}<i className="fa fa-question-circle"></i>
+                                </a>
+                                <Tooltip
+                                    anchorSelect='.support'
+                                    content='Friendly Forkhive Guides are standing by to answer your questions. Call +234 913 863 6045.'
+                                    style={{ width: '15rem' }}
+
+                                />
                             </td>
                             <td><i className="fa fa-check"></i></td>
                             <td><i className="fa fa-check"></i></td>
                         </tr>
 
                         <tr>
-                            <td><a data-tip data-for="online">Online Appointments and Reminders (Business Working hours) <i className="fa fa-question-circle"></i>
-                            </a>
-                                <Tooltip id="online">
-                                    <p>You can book one-time, recurring appointments, a webinar,<br /> class, workshop, in-person or virtual event directly from <br />your website and Remind customers of their upcoming bookings.</p>
-                                </Tooltip>
+                            <td>
+                                <a className="online">Online Appointments and Reminders (Business Working hours) 
+                                    {' '}<i className="fa fa-question-circle"></i>
+                                </a>
+                                <Tooltip
+                                    anchorSelect='.online'
+                                    content='You can book one-time, recurring appointments, a webinar, class, workshop, in-person or virtual event directly from your website and Remind customers of their upcoming bookings.'
+                                    style={{ width: '15rem' }}
+
+                                />
                             </td>
                             <td><i className="fa fa-check">Mon-Fri</i></td>
                             <td><i className="fa fa-check">Mon-Sun</i></td>
                         </tr>
 
                         <tr>
-                            <td><a data-tip data-for="ecommerce">Ecommerce Functionality <i className="fa fa-question-circle"></i>
-                            </a>
-                                <Tooltip id="ecommerce">
-                                    <p>Accept credit card, Paystack, Flutterwave, Interswitch Webpay, Paypal,<br /> Amplify and Remita payments for products/orders. Transaction fees<br /> are dependent on your selected payment processor. We don’t charge <br />additional fees and, with Paystack, offer one of the lowest transaction fees <br />(Local Cards: 1.5% + 100NGN, International Cards: 3.9% + NGN, <br />100 NGN 100 fee is waived for transactions less than NGN 2500) compared <br />to similar ecommerce plans.</p>
-                                </Tooltip>
+                            <td>
+                                <a className="ecommerce">Ecommerce Functionality
+                                    {' '}<i className="fa fa-question-circle"></i>
+                                </a>
+                                <Tooltip
+                                    anchorSelect='.ecommerce'
+                                    content='Accept credit card, Paystack, Flutterwave, Interswitch Webpay, Paypal, Amplify and Remita payments for products/orders. Transaction fees are dependent on your selected payment processor. We don’t charge additional fees and, with Paystack, offer one of the lowest transaction fees (Local Cards: 1.5% + 100NGN, International Cards: 3.9% + NGN, 100 NGN 100 fee is waived for transactions less than NGN 2500) compared to similar ecommerce plans.'
+                                    style={{ width: '15rem' }}
+
+                                />
                             </td>
                             <td><i className="fa fa-remove"></i></td>
                             <td><i className="fa fa-check"></i></td>
                         </tr>
 
                         <tr>
-                            <td><a data-tip data-for="database">Database Integration <i className="fa fa-question-circle"></i>
-                            </a>
-                                <Tooltip id="database">
-                                    <p>Does your website require database integration let us know</p>
-                                </Tooltip>
+                            <td>
+                                <a className="database">Database Integration
+                                    {' '}<i className="fa fa-question-circle"></i>
+                                </a>
+                                <Tooltip
+                                    anchorSelect='.database'
+                                    content='Does your website require database integration let us know'
+                                    style={{ width: '15rem' }}
+
+                                />
                             </td>
                             <td><i className="fa fa-remove"></i></td>
                             <td><i className="fa fa-check"></i></td>
                         </tr>
 
                         <tr>
-                            <td><a data-tip data-for="guidance">Guidance and Analytics <i className="fa fa-question-circle"></i>
-                            </a>
-                                <Tooltip id="guidance">
-                                    <p>If someone loads pages of your site on multiple different days,<br /> they will be counted as a unique visitor for each day. If someone<br /> loads pages of your site during multiple different hours, they will <br /> be counted as a unique visitor for each hour. You could be watching <br /> this metric</p>
-                                </Tooltip>
+                            <td>
+                                <a className="guidance">Guidance and Analytics
+                                    {' '}<i className="fa fa-question-circle"></i>
+                                </a>
+                                <Tooltip
+                                    anchorSelect='.guidance'
+                                    content='If someone loads pages of your site on multiple different days, they will be counted as a unique visitor for each day. If someone loads pages of your site during multiple different hours, they will  be counted as a unique visitor for each hour. You could be watching  this metric'
+                                    style={{ width: '15rem' }}
+
+                                />
                             </td>
                             <td><i className="fa fa-remove"></i></td>
                             <td><i className="fa fa-check"></i></td>
                         </tr>
 
                         <tr>
-                            <td><a data-tip data-for="seo">Search Engine Optimization (SEO) <i className="fa fa-question-circle"></i>
-                            </a>
-                                <Tooltip id="seo">
-                                    <p>Get found on Google and other search engines with help Gatsby or Netlify</p>
-                                </Tooltip>
+                            <td>
+                                <a className="seo">Search Engine Optimization (SEO)
+                                    {' '}<i className="fa fa-question-circle"></i>
+                                </a>
+                                <Tooltip
+                                    anchorSelect='seo'
+                                    content='Get found on Google and other search engines with help Gatsby or Netlify'
+                                    style={{ width: '15rem' }}
+
+                                />
                             </td>
                             <td><i className="fa fa-remove"></i></td>
                             <td><i className="fa fa-check"></i></td>
