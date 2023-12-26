fb78d6dcb4370ed6795a5c1db05f5366af07dc82
Bennyyoung <benjamineffiong124@yahoo.com>
2023-12-26 23:52:05 +0100

    feat: Persisted the dark mode using redux persist

--- wrap-with-provider.js
+++ wrap-with-provider.js
@@ -1,17 +1,20 @@
 import React from "react"
 import { Provider } from "react-redux"
-import store from "./src/redux/store"
+import { PersistGate } from "redux-persist/integration/react"
+import { store, persistor } from "./src/redux/store"
 
 // eslint-disable-next-line react/display-name,react/prop-types
 export default ({ element }) => {
     // Instantiating store in `wrapRootElement` handler ensures:
     //  - there is fresh store for each SSR page
     //  - it will be called only once in browser, when React mounts
-    
+
     return (
-        <Provider 
-        store={ store }>
-        {element}
+        <Provider
+            store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                {element}
+            </PersistGate>
         </Provider>
     )
 }
