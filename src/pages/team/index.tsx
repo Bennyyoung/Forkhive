76254d5ea0f81535480e5957cf8a2ed7f45ace64
Bennyyoung <benjamineffiong124@yahoo.com>
2024-02-28 08:25:46 +0100

    feature: updated the Team section to look nicer

--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -5,6 +5,8 @@ import Layout from '../../layouts/index';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
 import { Trans } from 'gatsby-plugin-react-i18next';
+import { Tooltip } from "react-tooltip";
+
 
 interface ITeam extends PageProps {
   data: {
@@ -46,12 +48,13 @@ const Team: React.FC<ITeam> = (props) => {
       </div>
 
       <div className="container pb-6">
-        <div className="row">
+      {/* <h4><Trans>Internal Team</Trans></h4> */}
+        <div style={{display: 'flex', textAlign: 'center'}}>
           {teams.map(edge => (
             <div key={edge.node.frontmatter.path} className="container col-12 col-md-6 mb-1">
               <div className="team card-two">
                 <div className="card-header">
-                  <div className="card-header-left">
+                  <div className="card-header-left" >
                     {edge.node.frontmatter.image && (
                       <div className="card-image">
                         <img
@@ -59,44 +62,12 @@ const Team: React.FC<ITeam> = (props) => {
                           className="img-fluid mb-2"
                           src={edge.node.frontmatter.image}
                         />
+                        <span className={`${darkMode ? 'dark-mode' : ''}`} style={{ fontWeight: '510' }}><Trans>{edge.node.frontmatter.jobtitle}</Trans></span>
                       </div>
                     )}
                   </div>
-                  <div className="card-right">
-                    <h2 className="card-title">{edge.node.frontmatter.title}</h2>
-                    <ul className="card-meta">
-                      <li style={{ paddingBottom: '4px' }}>
-                        <span className={`${darkMode ? 'dark-mode' : ''}`} style={{ fontWeight: '510' }}><Trans>{edge.node.frontmatter.jobtitle}</Trans></span>
-                        {/* <span style={{ fontWeight: '210' }}>{edge.node.frontmatter.subtitle}</span> */}
-                      </li>
-                      <li style={{ paddingBottom: '4px' }}>
-                        <span style={{ fontWeight: '210' }}><Trans>{edge.node.frontmatter.subtitle}</Trans></span>
-                      </li>
-                      {
-                        edge.node.frontmatter.linkedinurl && (
-                          <li className={`${darkMode ? 'dark-mode' : ''}`} style={{ paddingBottom: '4px' }}><b>LinkedIn:</b>
-                            <a target="_blank" href={edge.node.frontmatter.linkedinurl}>
-                              <Trans>{edge.node.frontmatter.linkedinurl}</Trans>
-                            </a>
-                          </li>
-                        )
-                      }
-                      <li className={`${darkMode ? 'dark-mode' : ''}`} style={{ paddingBottom: '4px' }}><b>Twitter:</b>
-                        <a target="_blank" href={edge.node.frontmatter.twitterurl}>
-                          <Trans>{edge.node.frontmatter.twitterurl}</Trans>
-                        </a>
-                      </li>
-                      <li className={`${darkMode ? 'dark-mode' : ''}`}><b><Trans>Email</Trans>:</b>
-                        <a href={`mailto: ${edge.node.frontmatter.email}`}><Trans>{edge.node.frontmatter.email}</Trans></a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div
-                  className="team-content"
-                  dangerouslySetInnerHTML={{ __html: edge.node.html }}
 
-                />
+                </div>
               </div>
             </div>
           ))}
